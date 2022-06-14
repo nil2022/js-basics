f05ea69206d5090fe6a5f86f09ae0ea596312346
Nilanjan H <105872070+nil2022@users.noreply.github.com>
2022-06-14 23:49:29 +0530

    bug fix-1

--- todoApp.js
+++ todoApp.js
@@ -1,8 +1,7 @@
 let todoApp = [],isDone = false;
-    let count= 1, n = todoApp.length;
+    let count= 1;
     const addTask = (task) => {
-        n = todoApp.length;
-        if(n!=0) ++count; //Increment task count if 1st task is added
+        if(todoApp.length!=0) ++count; //Increment task count if 1st task is added
         todoApp[count-1] = {id:count};
         todoApp[count-1].text = task;
         todoApp[count-1].isDone = isDone;
@@ -10,35 +9,19 @@ let todoApp = [],isDone = false;
         return '\n';
     }
     const deleteTask = () => {
-        //n = todoApp.length;
-        if(n==0){
+        if(todoApp.length==0){
             console.log(`No tasks available. Please add task first!`);  
         } else {
-            console.log(`Task${todoApp[n-1].id} deleted`);
+            console.log(`Task${todoApp[todoApp.length-1].id} deleted`);
             todoApp.pop();
         }
         return '\n';
     }
-    const updateTaskText = (id, text) => {
-        //n = todoApp.length;
-        if(n==0){
-            console.log(`No tasks available. Please add task first!`); 
-        }
-        else if((id>n || id<1) || typeof(id)==='undefined'){
-            console.log(`No tasks available. Please add task first!`);
-        } else {
-            todoApp[id-1].text = text;
-            console.log(`Task${id} updated`);
-            console.log(`Id:${todoApp[id-1].id} text: ${todoApp[id-1].text} isDone: ${todoApp[id-1].isDone}`);
-        }   
-        return '\n';     
-    } 
     const updateTaskAsDone = (id) => {
-        //n = todoApp.length;
-        if((id>n || id<1) || typeof(id)==='undefined'){
+          if((id>todoApp.length || id<1) || typeof(id)==='undefined'){
             console.log(`No tasks available. Please add task first!`);
         }
-        else if(todoApp[id-1].isDone==false){
+          else if(todoApp[id-1].isDone==false){
             console.log(`Task ${id} marked as done!`);
             todoApp[id-1].isDone = true;
         } else {
@@ -46,12 +29,21 @@ let todoApp = [],isDone = false;
         }
         return '\n';
     }
+    const updateTaskText = (id, text) => {
+        if((id>todoApp.length || id<1) || typeof(id)==='undefined'){
+            console.log(`No tasks available. Please add task first!`);
+        } else {
+            todoApp[id-1].text = text;
+            console.log(`Task${id} updated`);
+            console.log(`Id:${todoApp[id-1].id} text: ${todoApp[id-1].text} isDone: ${todoApp[id-1].isDone}`);
+        }
+            return '\n';
+        }    
     const getAllTasks = () => {
-        n = todoApp.length;
        if(todoApp.length==0){
            console.log('No tasks added!');
        } else {
-        for(i=0;i<n;i++){
+        for(i=0;i<todoApp.length;i++){
             console.log(`Id:${todoApp[i].id} text: ${todoApp[i].text} isDone: ${todoApp[i].isDone}`);
         }
        }
@@ -68,38 +60,31 @@ do{
             addTask(task1);
             flag=true;
             break;
-    case '2': if(n==0){
-              console.log(`No tasks available. Please add task first!`); 
-            } else {
-                deleteTask();
-                flag=true;
-            }     
+    case '2': deleteTask();
+              flag=true;     
               break;
-    case '3': if(n==0){
-              console.log(`No tasks available. Please add task first!`); 
+    case '3': if(todoApp.length==0){
+                console.log(`No tasks available. Please add task first!`);
             } else {
-                let id1 = prompt(`Enter Id to Mark done:`);             
-                updateTaskAsDone(id1);
-                flag=true;
-            }   break;
-    case '4': if(n==0){
-              console.log(`No tasks available. Please add task first!`); 
-            }else {
-                let id = 0, text = '';
+              let id1 = prompt(`Enter Id to Mark done:`);             
+              updateTaskAsDone(id1);
+              flag=true;
+            }
+              break;
+    case '4': if(todoApp.length==0){
+                console.log(`No tasks available. Please add task first!`); 
+            } else {
+              let id = 0, text = '';
               id = prompt(`Enter Id:`);
               text = prompt(`Enter text to update:`)
               updateTaskText(id,text);
               flag=true;
-            } 
+            }
               break;
-    case '5': if(n==0){
-              console.log(`No tasks available. Please add task first!`); 
-            }else {
-                getAllTasks();
-                flag=true;
-            }    
+    case '5': getAllTasks();
+              flag=true;  
               break;
-    default: console.log(`Exiting!`);
-             flag = false;
+    default : console.log(`Exiting!`);
+              flag = false;
  }
 }while (flag==true);
